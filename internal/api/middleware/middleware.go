75a7fa1d3ab1bbc3221c7cd2ff1bee2da570bb00
khomin <hominvladimir@gmail.com>
2026-07-17 00:49:00 +0300

    added bruno api

--- internal/api/middleware/middleware.go
+++ internal/api/middleware/middleware.go
@@ -21,32 +21,20 @@ func Auth(verifier *oidc.IDTokenVerifier) gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header format must be Bearer {token}"})
 			return
 		}
-
-		// tokenString := parts[1]
-		// userID := "auth0|65c8abc1234def5678" // This would come from token.Claims["sub"]
-		// c.Set("userID", userID)
-		// c.Next()
-
 		rawToken := parts[1]
 
-		// Verify the token
 		idToken, err := verifier.Verify(c.Request.Context(), rawToken)
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("Invalid token: %v", err)})
 			return
 		}
-
-		// Parse the claims
 		var claims map[string]interface{}
 		if err := idToken.Claims(&claims); err != nil {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse token claims"})
 			return
 		}
-
-		// Store the claims in the Gin context for later use
-		// This is how you access the user's info and roles downstream
 		c.Set("claims", claims)
-		c.Set("user_id", claims["sub"]) // Keycloak's user ID
+		c.Set("user_id", claims["sub"])
 		c.Set("email", claims["email"])
 		c.Next()
 	}
